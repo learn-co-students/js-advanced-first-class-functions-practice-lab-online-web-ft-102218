d5c435291d70a1a4c7ae20ce0ce7561c6ad4369b
kodyclemens <kody.clemens@gmail.com>
2019-02-06 20:40:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution in this file!
+function logDriverNames(array) {
+  array.forEach(function(value) {
+    console.log(value.name);
+  });
+}
+
+function logDriversByHometown(array, hometown) {
+  array.forEach(function(value) {
+    if (value.hometown === hometown) {
+      console.log(value.name);
+    }
+  })
+}
+
+function driversByRevenue(array) {
+  return array.slice().sort(function(x, y) {
+    return x.revenue - y.revenue;
+  });
+}
+
+function driversByName(array) {
+  return array.slice().sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+}
+
+function totalRevenue(array) {
+  return array.reduce(function(total, driver) {
+    return total + driver.revenue;
+  }, 0);
+}
+
+function averageRevenue(array) {
+  return totalRevenue(array) / array.length;
+}
+
+// const drivers = [
+//     { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
+//     { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
+//     { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
+//     { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
+//     { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
+//   ];
+
+// driversByRevenue(drivers);
